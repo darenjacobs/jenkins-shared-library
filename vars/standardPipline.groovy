4084c225e81d6388e1348a7b86812ca77e87334b
darenjacobs <darenjacobs@msn.com>
2018-03-09 17:05:29 +0000

    add pipeline

---
+++ vars/standardPipline.groovy
@@ -0,0 +1,33 @@
+def call(body) {
+
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+
+        node ('jenkins-master-01'){
+            // Clean workspace before doing anything
+            deleteDir()
+
+            try {
+                stage ('Clone') {
+                    checkout scm
+                }
+                stage ('Build') {
+                    sh "echo 'building ${config.projectName} ...'"
+                    sh "mvn clean compile package"
+                }
+                stage ('Tests') {
+                    sh "echo 'shell scripts to run static tests...'"
+                    sh "mvn test"
+                    sh "mvn verify"
+                }
+                stage ('Deploy') {
+                    sh "echo 'deploying to server ${config.serverDomain}...'"
+                }
+            } catch (err) {
+                currentBuild.result = 'FAILED'
+                throw err
+            }
+        }
+    }
