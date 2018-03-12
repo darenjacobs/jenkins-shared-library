c07b5ebcc713cba441d0c1eac02a72f73961fffe
darenjacobs <darenjacobs@msn.com>
2018-03-12 12:37:27 +0000

    jenkins shared lib

---
+++ vars/pl_02.groovy
@@ -0,0 +1,34 @@
+def call(body) {
+
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+
+        node {
+            // Clean workspace before doing anything
+            deleteDir()
+
+            try {
+                stage ('Clone') {
+                    checkout scm
+                }
+                stage ('Build') {
+                    sh "echo 'building ${config.projectName} ...'"
+                    sh "mvn clean compile package"
+                }
+                stage ('Tests') {
+                    sh "echo 'shell scripts to run static tests...'"
+                    sh "mvn test"
+                    sh "mvn verify"
+                }
+                stage ('Deploy') {
+                    sh "echo 'deploying to server ${config.serverDomain}...'"
+                    sh "mvn deploy"
+                }
+            } catch (err) {
+                currentBuild.result = 'FAILED'
+                throw err
+            }
+        }
+    }
