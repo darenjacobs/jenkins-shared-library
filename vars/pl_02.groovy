950c78a49ffca98657f00289ef89db858c664c52
darenjacobs <darenjacobs@msn.com>
2018-07-24 11:55:49 -0400

    update

--- vars/pl_02.groovy
+++ vars/pl_02.groovy
@@ -5,7 +5,7 @@ def call(body) {
         body.delegate = config
         body()
 
-        node (label: 'jenkins-node') {
+        node (label: 'swarm') {
             // Clean workspace before doing anything
             deleteDir()
 
