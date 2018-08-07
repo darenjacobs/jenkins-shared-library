a94d16e15f1c8d090bdb8cfd3b4dfa986a020355
darenjacobs <darenjacobs@msn.com>
2018-08-07 12:19:22 -0400

    pl_01.groovy

--- vars/pl_01.groovy
+++ vars/pl_01.groovy
@@ -5,7 +5,7 @@ def call(body) {
         body.delegate = config
         body()
 
-        node (label: 'swarm') {
+        node (label: 'alpine') {
             // Clean workspace before doing anything
             deleteDir()
 
